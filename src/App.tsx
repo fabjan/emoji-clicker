765dcc1d2dbf5a009edac52fb2842ee5fb59c389
Fabian <fabian@fmbb.se>
2021-06-03 15:12:15 +0200

    clean up skeleton, add license and bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Jumbotron } from 'reactstrap';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jumbotron>
+        <h1 className="display-3">Hejsan världen!</h1>
+      </Jumbotron>
     </div>
   );
 }
